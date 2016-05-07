ce6d003f87a9e3b57ed730ca365ed3cb5c3d6a8c
Kai Garrott <garrottkai@gmail.com>
2016-05-07 14:09:58 -0600

    update

--- app/app.vote-abq.ts
+++ app/app.vote-abq.ts
@@ -2,14 +2,14 @@ import {Component} from "angular2/core";
 import {VotingList} from "./voting-list";
 import {HTTP_PROVIDERS} from "angular2/http";
 import {VotingService} from "./voting.service";
-import {RouteConfig, ROUTER_DIRECTIVES} from "angular2/router";
+import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from "angular2/router";
 
 
 @Component({
 	selector: "vote-abq",
 	templateUrl: "app/templates/splash-page.html",
-	directives: [VotingList],
-	providers: [HTTP_PROVIDERS, VotingService]
+	directives: [ROUTER_DIRECTIVES, VotingList],
+	providers: [HTTP_PROVIDERS, ROUTER_PROVIDERS, VotingService]
 })
 
 @RouteConfig([
--- app/main.ts
+++ app/main.ts
@@ -3,5 +3,6 @@ import {HTTP_PROVIDERS} from "angular2/http";
 import "rxjs/Rx";
 import {VoteAbq} from "./app.vote-abq";
 import {ANGULAR2_GOOGLE_MAPS_PROVIDERS} from "angular2-google-maps/core";
+import {ROUTER_PROVIDERS} from "angular2/router";
 
-bootstrap(VoteAbq, [ANGULAR2_GOOGLE_MAPS_PROVIDERS]);
+bootstrap(VoteAbq, [ANGULAR2_GOOGLE_MAPS_PROVIDERS, ROUTER_PROVIDERS]);
